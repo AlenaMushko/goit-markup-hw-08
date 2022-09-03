b1a200b58689ed8947cec20c45a3ec3f672cfb5e
alena <evstafienko_a@ukr.net>
2022-09-03 09:41:41 +0300

    no foto

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,4 @@
-(() => {
+/*(() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
 
@@ -11,4 +11,21 @@
 
     mobileMenuRef.classList.toggle("is-open");
   });
+})();    */
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
 })();
